04ce135c2a8b64f2064622a56619a1e7d688894b
chetanairo <chetan.musale@airodigitallabs.com>
2023-02-08 17:58:18 +0530

    changing the logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../css/Header.css";
+import Logo from "../assets/airo-logo.png";
 import SearchBar from './SearchBar';
 
 export const Header = ({keyword, onChange, fetchData}) => {
     return (
         <header className="header">
-            <img src={require("../assets/airo-logo.png")} className="logo" alt="logo"/>
+            <img src={Logo} className="logo" alt="logo"/>
             <SearchBar {...{ keyword, onChange, fetchData }} />
         </header>
     )
